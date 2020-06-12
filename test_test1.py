b0d1cfb2a077c5da38a4bfb099e13ce5b7677816
Eric Fulmer <eric.r.fulmer@gmail.com>
2020-06-12 09:54:16 -0400

    BUG: pytest fails because there are no tests

--- test_test1.py
+++ test_test1.py
@@ -1,1 +1,5 @@
-print('Hello World')
+def test_that_always_works():
+    """This is a test that will never fail. Exists so that pytest does
+    not error because there are no tests.
+    """
+    assert True
